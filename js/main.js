033ab246ab43cbf974ca1feaa28ca64d78674696
Jeffrey Su <sujeff@wustl.edu>
2020-08-21 20:33:35 -0500

    Update

---
+++ js/main.js
@@ -0,0 +1,3 @@
+let imgHeight = document.getElementsByClassName('overlap-img')[0].offsetHeight;
+let spacerDiv = document.getElementsByClassName('spacer')[0];
+spacerDiv.style.height = imgHeight - 65 + "px";
